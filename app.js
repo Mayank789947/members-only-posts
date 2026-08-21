7e7ad90464e9611f1a8d882f3dd8a88df9b2d55c
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-08-21 12:51:59 +0530

    feat: Add user model with CRUD operations
    - add user model with CRUD operations with our database

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const errorHandler = require("./src/middlewares/errorMiddlewares/errorHandler");
+const userRouter = require("./src/routes/userRouter");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
+
 
 app.use((req, res, next) => {
     next(new NotFoundError("Route not found"));
---
+++ src/models/userModel.js
@@ -0,0 +1,91 @@
+const pool = require("../db/pool");
+
+async function getUserByEmail(email) {
+    const result = await pool.query(
+        `
+          SELECT id,
+          first_name,
+          last_name,
+          email,
+          password_hash,
+          username,
+          membership_status,
+          is_admin
+           FROM users
+           WHERE email = $1;
+        `,
+        [email]
+    );
+
+    return result.rows[0] || null;
+}
+
+async function createUser(first_name, last_name, email, username,  password_hash) {
+    const result = await pool.query(
+        `
+         INSERT INTO users
+         (first_name, last_name, email, username, password_hash)
+         VALUES
+         ($1, $2, $3, $4, $5)
+         RETURNING id, email;
+        `,
+        [first_name, last_name, email, username, password_hash]
+    );
+
+    return result.rows[0] || null;
+}
+
+async function getUserById(userId) {
+    const result = await pool.query(
+        `
+         SELECT id,
+          first_name,
+          last_name,
+          email,
+          password_hash,
+          username,
+          membership_status,
+          is_admin
+            FROM users
+            WHERE id = $1;
+        `,
+        [userId]
+    );
+
+    return result.rows[0] || null;
+}
+
+async function updateMembershipStatus(status, userId) {
+    const result = await pool.query(
+        `
+           UPDATE users
+            SET membership_status = $1
+             WHERE id = $2
+             RETURNING membership_status;
+        `,
+        [status, userId]
+    );
+
+    return result.rows[0] || null;
+}
+
+async function deleteUser(userId) {
+    const result = await pool.query(
+        `
+          DELETE FROM users
+           WHERE id = $1
+           RETURNING id;
+        `,
+        [userId]
+    );
+
+    return result.rows[0] || null;
+}
+
+module.exports = {
+    getUserByEmail,
+    createUser,
+    getUserById,
+    updateMembershipStatus,
+    deleteUser
+}
