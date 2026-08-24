427136fa9feb6800093aafed71f6024905addcba
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-08-24 18:33:32 +0530

    feat: Add static file serving middleware for public directory
    - Configure app.js file to serve static files in our app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const NotFoundError = require("./src/errors/NotFoundError");
 const passport = require("passport");
 const authRouter = require("./src/routes/authRouter");
 require("./src/config/passport");
+const path = require("path");
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
